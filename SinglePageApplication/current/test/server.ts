3c2f5be9fe1b56a7b4a0ecd04d8457875ede9d8b
Daniel Rodriguez <Daniel.Rodriguez@microsoft.com>
2022-03-15 19:24:57 +0000

    wip

--- SinglePageApplication/current/test/server.ts
+++ SinglePageApplication/current/test/server.ts
@@ -96,45 +96,6 @@ export async function prepareServer(
   );
 
   /**
-   * Logout just deletes the user from the session.
-   */
-  app.get("/logout", async (req: express.Request, res: express.Response) => {
-    const session = (req as ExpressRequestWithSession).session;
-    const username = session.username;
-    database[username].loggedIn = false;
-    delete session.username;
-    res.sendStatus(200);
-  });
-
-  /**
-   * To login is just to set the user on the session.
-   * We don't use passwords because this is a test environment.
-   */
-  app.get("/login", async (req: express.Request, res: express.Response) => {
-    let username = extractUsername(req);
-    if (username) {
-      console.error(`Already authenticated username ${username}`);
-      res.write("User is already authenticated");
-      res.sendStatus(500);
-      return;
-    }
-
-    username = req.query.username as string;
-    const session = (req as ExpressRequestWithSession).session;
-    session.username = username;
-
-    if (!database[username]) {
-      database[username] = {
-        loggedIn: true,
-        azure: {},
-      };
-    }
-
-    database[username].loggedIn = true;
-    res.sendStatus(200);
-  });
-
-  /**
    * Home page
    */
   app.get(
