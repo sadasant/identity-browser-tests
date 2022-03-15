dc4478412d51229f962a73b3809ed1667bfee659
Daniel Rodriguez <Daniel.Rodriguez@microsoft.com>
2022-03-15 19:23:48 +0000

    wip

---
+++ SinglePageApplication/current/test/server.ts
@@ -0,0 +1,203 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+import { AccessToken, TokenCredential } from "@azure/core-auth";
+import * as express from "express";
+import { Server } from "http";
+import * as session from "express-session";
+import fs from "fs";
+
+// A simple web server that allows passing configuration and behavioral parameters.
+// This file should be thought as the archetypicall representation of a web server,
+// whereas the test file will include the nuance specific to testing the desired behavior.
+
+/**
+ * Simplified type representing an Express request with a session.
+ */
+type ExpressRequestWithSession = express.Request & {
+  session: {
+    username: string;
+  };
+};
+
+/**
+ * Representative of the state relative to the Azure authentication
+ */
+export interface AzureState {
+  accessToken?: AccessToken;
+  credential?: TokenCredential;
+}
+
+/**
+ * Simple representation of in-memory state with a user.
+ */
+interface UserState {
+  loggedIn: boolean;
+  azure: AzureState;
+}
+
+/**
+ * Options to the server.
+ * With the intent to make the server parametrizable!
+ */
+export interface ServerOptions {
+  /**
+   * Secret used for the server session.
+   */
+  serverSecret: string;
+  /**
+   * Port number as a string
+   */
+  port: string;
+}
+
+/**
+ * Result of the prepareServer function.
+ */
+export interface PepareServerResult {
+  app: express.Application;
+  database: Record<string, UserState>;
+  extractUsername: (req: express.Request) => string;
+  checkLoggedIn: (username: string) => void;
+  extractToken: (username: string) => string;
+  saveAzureState: (username: string, options: AzureState) => void;
+  start: () => Promise<void>;
+  stop: () => Promise<void>;
+}
+
+/**
+ * Sets up a parametrizable Express server.
+ */
+export async function prepareServer(
+  serverOptions: ServerOptions
+): Promise<PepareServerResult> {
+  const app = express();
+  const database: Record<string, UserState> = {};
+  const { port, serverSecret } = serverOptions;
+
+  function extractUsername(req: express.Request) {
+    return (req as ExpressRequestWithSession).session.username;
+  }
+
+  app.use(session({ secret: serverSecret }));
+
+  /**
+   * Logging calls.
+   */
+  app.use(
+    (
+      req: express.Request,
+      res: express.Response,
+      next: express.NextFunction
+    ) => {
+      console.log("Server:", req.url);
+      next();
+    }
+  );
+
+  /**
+   * Logout just deletes the user from the session.
+   */
+  app.get("/logout", async (req: express.Request, res: express.Response) => {
+    const session = (req as ExpressRequestWithSession).session;
+    const username = session.username;
+    database[username].loggedIn = false;
+    delete session.username;
+    res.sendStatus(200);
+  });
+
+  /**
+   * To login is just to set the user on the session.
+   * We don't use passwords because this is a test environment.
+   */
+  app.get("/login", async (req: express.Request, res: express.Response) => {
+    let username = extractUsername(req);
+    if (username) {
+      console.error(`Already authenticated username ${username}`);
+      res.write("User is already authenticated");
+      res.sendStatus(500);
+      return;
+    }
+
+    username = req.query.username as string;
+    const session = (req as ExpressRequestWithSession).session;
+    session.username = username;
+
+    if (!database[username]) {
+      database[username] = {
+        loggedIn: true,
+        azure: {},
+      };
+    }
+
+    database[username].loggedIn = true;
+    res.sendStatus(200);
+  });
+
+  /**
+   * Home page
+   */
+  app.get(
+    "/",
+    async (req: express.Request, res: express.Response) => {
+      const identitySource = fs.readFileSync("../node_modules/@azure/identity/dist/index.js", { encoding: "utf8" });
+      res.send(`
+<html>
+  <head>
+    <script>${identitySource}</script>
+  </head>
+  <body>
+  </body>
+</html>
+`)
+    }
+  );
+
+  /**
+   * Azure Redirect
+   */
+  app.get(
+    "/azureResponse",
+    async (req: express.Request, res: express.Response) => {
+      const identitySource = fs.readFileSync("../node_modules/@azure/identity/dist/index.js", { encoding: "utf8" });
+      res.send(`
+<html>
+  <head>
+    <script>${identitySource}</script>
+  </head>
+  <body>
+  </body>
+</html>
+`)
+    }
+  );
+ 
+  let server: Server | undefined = undefined;
+
+  return {
+    app,
+    database,
+    extractUsername,
+    extractToken: (username: string): string => {
+      return database[username].azure?.accessToken.token;
+    },
+    checkLoggedIn: (username: string) => {
+      if (!(username && database[username].loggedIn)) {
+        throw new Error(`Unauthorized username ${username}`);
+      }
+    },
+    saveAzureState: async (username: string, options: AzureState) => {
+      database[username].azure = options;
+    },
+    async start() {
+      server = app.listen(serverOptions.port, () => {
+        console.log(
+          `Authorization code redirect server listening on port ${serverOptions.port}`
+        );
+      });
+    },
+    async stop() {
+      server.close();
+    },
+  };
+}
---
+++ SinglePageApplication/current/test/test.spec.ts
@@ -0,0 +1,156 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+import {
+  AccessToken,
+  AuthorizationCodeCredential,
+  TokenCredential,
+} from "@azure/identity";
+import {
+  createDefaultHttpClient,
+  createHttpHeaders,
+  createPipelineRequest,
+} from "@azure/core-rest-pipeline";
+import { delay } from "@azure/core-util";
+import { test, expect } from "@playwright/test";
+import { prepareServer } from "./server";
+import { credentialWrapper, sendRequest } from "./wrappers";
+import { getAuthorizeUrl } from "./utils";
+import * as express from "express";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+// This test shows how to authenticate a browser using the SinglePageApplicationCredential.
+// Throughout this document, we'll point out to the challenges of the existing approach with comment sections
+// that will begin with "CHALLENGE", for example:
+//
+//     CHALLENGE (n of N):
+//     <explanation-of-the-challenge>
+//
+// Challenges:
+// 1. `loginStyle` changes authentication drastically.
+// 2. It might be weird that getToken completely obliterates the currently running program (by redirecting).
+// 3. After redirection, the credential retrieves the code from the URL, which might be confusing since this is hidden from the user.
+// 4. How to handle multiple users in the browser?
+// 5. No "state" parameter.
+// 6. No way to log out.
+
+const tenantId = process.env.AZURE_TENANT_ID;
+const clientId = process.env.AZURE_CLIENT_ID;
+const clientSecret = process.env.AZURE_CLIENT_SECRET;
+const serverSecret = process.env.SERVER_SECRET;
+const azureUsername = process.env.AZURE_USERNAME;
+const azurePassword = process.env.AZURE_PASSWORD;
+const protocol = process.env.PROTOCOL || "http";
+const host = process.env.HOST || "localhost";
+const port = process.env.PORT;
+const scope = "https://graph.microsoft.com/.default";
+const homeUri = `http://localhost:${port}`;
+const authorizeHost =
+  process.env.AUTHORIZE_HOST ||
+  `https://login.microsoftonline.com/${tenantId}/oauth2/v2.0`;
+const clearState = process.env.CLEAR_STATE;
+
+// The Azure Active Directory app registration should be of the type
+// "web" and the redirect endpoint should point to:
+const redirectUri = `${protocol}://${host}:${port}/azureResponse`;
+
+test("Authenticates", async ({ page }) => {
+  const {
+    app,
+    database,
+    extractUsername,
+    extractToken,
+    checkLoggedIn,
+    saveAzureState,
+    start,
+    stop,
+  } = await prepareServer({ serverSecret, port });
+
+ await start();
+
+  // Log and continue all network requests
+  await page.route("**", (route) => {
+    console.log(route.request().url());
+    route.continue();
+  });
+
+  // THE TEST BEGINS
+
+  const username = "testuser";
+
+  // We go to the home page
+  await page.goto(`${homeUri}/`);
+  await expect(loginResponse.status(), "The home page request should have succeeded").toBe(200);
+
+  if (clearState) {
+    // Create state in the web page
+    await page.evaluate(() => {
+      window.localStorage.page = undefined;
+    });
+  }
+
+  // Create state in the web page
+  await page.evaluate(() => {
+    if (!window.localStorage.pageState) {
+      window.localStorage.page = {
+        steps: 0,
+        timeout: 1000
+      }
+    }
+    setTimeout(() => {
+      window.localStorage.page.steps += 1;
+    }, window.localStorage.page.timeout);
+  });
+
+  // Authenticate
+  await page.evaluate(() => {
+    const credential = new InteractiveBrowserCredential({
+      clientId,
+      // CHALLENGE (1 of 6):
+      // 1. `loginStyle` changes authentication drastically.
+      // // loginStyle: "popup"
+    });
+   
+    credential.getToken(scope);
+  });
+
+  // Waiting for redirection...
+  await page.waitForNavigation({ url: '**/azureResponse' });
+
+  await page.evaluate(async () => {
+    // CHALLENGE (2 of 6):
+    // 2. It might be weird that getToken completely obliterates
+    // the currently running program (by redirecting).
+
+    // CHALLENGE (3 of 6):
+    // 3. After redirection, the credential retrieves
+    // the code from the URL, which might be confusing
+    // since this is hidden from the user.
+
+    // CHALLENGE (4 of 6):
+    // 4. How to handle multiple users in the browser?
+    // At the moment, there's now way to know what user authenticated,
+    // and how to manage multiple users over time.
+    // Every time one authenticates, that user becomes
+    // (apparently) the only user authenticated.
+
+    const credential = new InteractiveBrowserCredential({
+      clientId
+    });
+    const token = await credential.getToken(scope);
+
+    // CHALLENGE (5 of 6):
+    // 5. No "state" parameter.
+    // `getToken` will route to the Azure page that authenticates,
+    // and after we come back from the redirection,
+    // we won't be able to know at what step of the "state"
+    // the redirection happened.
+
+    // CHALLENGE (6 of 6):
+    // 6. No way to log out.
+  });
+
+  await stop();
+});
